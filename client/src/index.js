f1c8f1c1a53ee812d1ee84db8fcf63b648b94750
Igor Zaika <izaika89@gmail.com>
2017-11-06 23:43:23 +0200

    API keys for client

--- client/src/index.js
+++ client/src/index.js
@@ -16,3 +16,6 @@ ReactDOM.render(
   <Provider store={store}><App/></Provider>,
   document.getElementById('root')
 );
+
+console.log(`stripe key is: ${process.env.REACT_APP_STRIPE_KEY}`);
+console.log(`env is: ${process.env.NODE_ENV}`);
