42a4f732872ecd25fdb65d5b0e66f1863127f872
Igor Zaika <izaika89@gmail.com>
2017-11-14 23:47:44 +0200

    Heroku deploy prepare

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
